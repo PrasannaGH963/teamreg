c09fcbae4897def5cd3dc641358ee6a5331200f9
PrasannaGH963 <prasannabadgujar963@gmail.com>
2024-01-19 23:24:03 +0530

    gamestorm id verification done braniac about

--- pages/Braniac.py
+++ pages/Braniac.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.title('Team Registration for Treasure Trove')
+st.title('Time-Warped Brainiac: Teams, Rewind Your Maps!')
+
+st.markdown("## Brainiac Challenge Solo Participants:")
+st.markdown("🚀 Greetings Time-Traveling Brainiac! 🧠⏳")
+st.markdown("Whoa! You've bravely embarked on the Brainiac Challenge solo journey. "
+                "No need for a sidekick when you're the superhero of intellect! 🦸‍♂💡\n\n"
+                "Prepare for mind-bending aptitude tests, intellectual debates, and a mock interview "
+                "(don't worry, the interviewers are nice time-traveling folks). "
+                "Your solo mission: to conquer brains across the space-time continuum! 🌌✨\n\n"
+                "Solo maestro, your wit is the DeLorean, and you're about to hit 88 mph into "
+                "the solo adventure of a lifetime!\n\n"
+                "P.S. Remember, in the time-traveling circus of brilliance, you're the solo acrobat! 🎩🌟")
--- pages/Gamestrom.py
+++ pages/Gamestrom.py
@@ -13,7 +13,9 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("litforms.json", scope)
 client = gspread.authorize(creds)
 
 # Replace 'Sheet Name' with your actual sheet name
-sheet = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("Sheet8")
+sheet_er = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("Sheet8")
+sheet_tr = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("GS TR")
+
 
 st.title("Team Registration for Gamestorm")
 search_id = st.text_input("Enter your ID:")
@@ -21,52 +23,75 @@ search_id = st.text_input("Enter your ID:")
 if search_id:
 
     # Fetch entire row corresponding to the ID
-    cell = sheet.find(search_id)
-    if cell:
-        row_values = sheet.row_values(cell.row)
-        name_str = row_values[cell.col]
-        st.write(f"Hallo, {name_str} 👋!")
+# Check in sheet_tr
+    cell_tr = sheet_tr.find(search_id)
 
-        game_str = row_values[cell.col + 7]
+    if cell_tr:
+        st.warning("Team already registered")
+    else:
+        # Check in sheet_er
+        cell_er = sheet_er.find(search_id)
+
+        if cell_er:
+            st.title("")
+        else:
+            st.warning("ID not found")
+        row_values = sheet_er.row_values(cell_er.row)
+        name_str = row_values[cell_er.col]
+        st.write(f"Hello, {name_str} 👋!")
+
+        game_str = row_values[cell_er.col + 7]
         game_list = game_str.split(",") if game_str else []
+
+        # Add a string before each element in game_list
+        formatted_game_list = [f"GameStorm: {game}" for game in game_list]
+
+        
+        
+
         st.write(f"Game for ID {search_id}:")
-        st.write(game_list)
+        st.write(formatted_game_list)
 
         with st.form("Team_Reg"):
             teammate_info_list = []
             for game in game_list:
                 st.title(f"Teammates for : {game}")
 
-                # Dynamically set the number of teammates based on the game
-                if game in ["BGMI", "FreeFire"]:
-                    game_modes = ["Duo", "Squad"]
-                    selected_mode = st.selectbox(f"Select the game mode for {game}:", game_modes, key=f"{game}_mode")
-                    num_mapping = {"Duo": 2, "Squad": 4}
-                    num = num_mapping[selected_mode]
+                if game == ["BGMI","FreeFire"]:
+                    num = 4  # Default number of teammates for other games
                 else:
-                    num = 5  # Default number of teammates for other games
+                    num=5
+
+                teammate_names = []
+                teammate_numbers = []
+                teammate_emails = []
 
                 for i in range(num):
-                    
                     teammate_name = st.text_input(f"Name of Teammate {i + 1}:", key=f"{game}_name_{i}")
                     teammate_number = st.text_input(f"Number of Teammate {i + 1}:", max_chars=10,
                                                     key=f"{game}_num_{i}")
                     teammate_email = st.text_input(f"Email of Teammate {i + 1}:", key=f"{game}_em_{i}")
 
-                    teammate_info = {
-                        'ID': search_id,
-                        'For Event': game,
-                        'Name': teammate_name,
-                        'Number': teammate_number,
-                        'Email': teammate_email
-                    }
+                    teammate_names.append(teammate_name)
+                    teammate_numbers.append(teammate_number)
+                    teammate_emails.append(teammate_email)
 
-                    teammate_info_list.append(teammate_info)
+                # Concatenate teammate info with commas
+                teammate_info = {
+                    'ID': search_id,
+                    'For Event': game,
+                    'Names': ', '.join(teammate_names),
+                    'Numbers': ', '.join(teammate_numbers),
+                    'Emails': ', '.join(teammate_emails)
+                }
+
+                teammate_info_list.append(teammate_info)
 
             submit_button = st.form_submit_button("Submit")
             if submit_button:
                 df = pd.DataFrame(teammate_info_list)
-                w = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("TeamReg")
+                print(teammate_info_list)
+                w = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("GS TR")
                 last = len(w.col_values(1)) + 1
                 set_with_dataframe(w, df, row=last, include_index=False, include_column_header=False)
                 st.success("Team Registered! ✨")
